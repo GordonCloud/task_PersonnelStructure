573ffc105ae5ea76040b6015f906efeae83ca3de
gordonbillycloud@inbox.ru <gordonbillycloud@inbox.ru>
2022-05-16 11:26:01 +1200

    Maked viev and url of the main page

--- employees/views.py
+++ employees/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+from .models import Employee
+
+
+def page(request):
+    all_employees = Employee.objects.all()
+    template = loader.get_template('employees/page.html')
+    context = {
+        'AllEmployees': all_employees,
+    }
+    return HttpResponse(template.render(context, request))
