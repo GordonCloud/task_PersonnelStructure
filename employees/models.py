a4a5d280aec0c057c2d061e1f7de2a6632cc1a69
gordonbillycloud@inbox.ru <gordonbillycloud@inbox.ru>
2022-05-20 11:53:48 +1200

    Added field hierarchy_level in model Employee

--- employees/models.py
+++ employees/models.py
@@ -8,4 +8,8 @@ class Employee(models.Model):
     position = models.TextField(max_length=100)
     employment_date = models.DateField()
     salary = models.IntegerField()
+    hierarchy_level = models.IntegerField(default=5)
     manager = models.ForeignKey('self', null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.first_name
