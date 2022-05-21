557085edf21d05a2ffd43e92380efc05ff56bbdd
gordonbillycloud@inbox.ru <gordonbillycloud@inbox.ru>
2022-05-21 17:39:27 +1200

    The main page template has been improved, now employees of the first and second levels of the hierarchy are displayed there

--- employees/views.py
+++ employees/views.py
@@ -5,9 +5,11 @@ from .models import Employee
 
 
 def page(request):
-    all_employees = Employee.objects.all()
+    employee_director = Employee.objects.get(hierarchy_level=1)
+    second_level_employees = Employee.objects.filter(hierarchy_level=2)
     template = loader.get_template('employees/page.html')
     context = {
-        'AllEmployees': all_employees,
+        'employee_director': employee_director,
+        'second_level_employees': second_level_employees,
     }
     return HttpResponse(template.render(context, request))
