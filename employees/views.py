7ff5dd7a9316bd7850fc7844d72841bd999b63fd
gordonbillycloud@inbox.ru <gordonbillycloud@inbox.ru>
2022-06-13 16:34:13 +1200

    Some changes to template page,html

--- employees/views.py
+++ employees/views.py
@@ -7,9 +7,12 @@ from .models import Employee
 def page(request):
     employee_director = Employee.objects.get(hierarchy_level=1)
     second_level_employees = Employee.objects.filter(hierarchy_level=2)
+    employee_tree = Employee.get_tree
     template = loader.get_template('employees/page.html')
     context = {
         'employee_director': employee_director,
         'second_level_employees': second_level_employees,
+        'firm_name': "ОАО Маркиса Де Коте",
+        'employee_tree': employee_tree
     }
     return HttpResponse(template.render(context, request))
