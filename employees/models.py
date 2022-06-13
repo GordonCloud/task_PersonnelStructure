b9c3bbe22838e29751f6ab12a62d960ed825d767
gordonbillycloud@inbox.ru <gordonbillycloud@inbox.ru>
2022-06-13 16:18:44 +1200

    Changed models.py. Added method get_tree that creates ordered list for employees view

--- employees/models.py
+++ employees/models.py
@@ -1,4 +1,6 @@
+from django.db.models.query import QuerySet
 from django.db import models
+from django.db.models import Max
 
 
 class Employee(models.Model):
@@ -11,6 +13,23 @@ class Employee(models.Model):
     hierarchy_level = models.IntegerField(default=5)
     manager = models.ForeignKey('self', null=True, on_delete=models.SET_NULL)
 
+    employees_tree = []
+
     def __str__(self):
-        return self.last_name + " " + self.first_name +\
+        return self.last_name + " " + self.first_name + \
                " " + self.middle_name
+
+    @classmethod
+    def get_tree(cls):
+        second_level_queryset = cls.objects.filter(hierarchy_level=2)
+        for employee in second_level_queryset:
+            cls.employees_tree.append(employee)
+            manager = employee
+            cls.add_subordinates_to_tree(manager=manager)
+
+    @classmethod
+    def add_subordinates_to_tree(cls, manager):
+        subordinates = cls.objects.filter(manager=manager)[:5]
+        for employee in subordinates:
+            cls.employees_tree.append(employee)
+            cls.add_subordinates_to_tree(employee)
