eb0fb945d9be8c25e6fbc4146912001ce8d7c26c
gordonbillycloud@inbox.ru <gordonbillycloud@inbox.ru>
2022-07-17 18:12:04 +1200

    Added url and view for the search page

--- employees/views.py
+++ employees/views.py
@@ -30,3 +30,18 @@ def page(request):
         'tr_tag_colors': tr_tag_colors
     }
     return HttpResponse(template.render(context, request))
+
+
+def searchpage(request):
+    employee_director = Employee.objects.get(hierarchy_level=1)
+    if not Employee.employees_tree:
+        employee_tree = Employee.get_tree()
+    else:
+        employee_tree = Employee.employees_tree
+    template = loader.get_template('employees/searchpage.html')
+    context = {
+        'employee_director': employee_director,
+        'firm_name': "ОАО Маркиса Де Коте",
+        'employee_tree': employee_tree,
+    }
+    return HttpResponse(template.render(context, request))
