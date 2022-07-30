fe872d05d824d307512ff787c033872ff544692d
gordonbillycloud@inbox.ru <gordonbillycloud@inbox.ru>
2022-07-30 21:53:16 +1200

    Renamed mainpage url. Added url for POST request from the search form

--- employees/urls.py
+++ employees/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.page, name='page'),
+    path('', views.page, name='mainpage'),
     path('search/', views.searchpage, name='searchpage'),
+    path('search/new/', views.post_searchform, name='post_searchform'),
 ]
