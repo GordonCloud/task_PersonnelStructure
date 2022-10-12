0e69b9970358a877f60a924888ee9f442eb67ddc
Severian <gordoncloud@inbox.ru>
2022-10-12 21:25:46 +0300

    Added view and html for the search

--- employees/models.py
+++ employees/models.py
@@ -23,17 +23,17 @@ class EmployeeTree:
 
     def __init__(self, employees):
         self._employees = employees
-        self.employees_tree = []
+        self.current_tree = []
 
-        self.get_tree()
+        self._get_tree()
 
-    def get_tree(self):
+    def _get_tree(self):
         upper_level_employees_list = self._get_upper_lvl_from_queryset()
         for employee in upper_level_employees_list:
-            self.employees_tree.append(employee)
+            self.current_tree.append(employee)
             manager = employee
             self._add_subordinates_to_tree(manager=manager)
-        return self.employees_tree
+        return self.current_tree
 
     def _get_upper_lvl_from_queryset(self):
         upper_lvl = int(100)
@@ -49,12 +49,14 @@ class EmployeeTree:
                 upper_lvl = employee.hierarchy_level
         return upper_level_employees_list
 
+    #ToDo: ограничение количества подчиненных. Нужно, чтобы оно не влияло на параметр
+    # employee_tree
     def _add_subordinates_to_tree(self, manager):
-        subordinates = list(Employee.objects.filter(manager=manager)[:5])
+        subordinates = list(Employee.objects.filter(manager=manager)[:50])
         subordinates_filtered = list(set(subordinates) & set(self._employees))
         for employee in subordinates_filtered:
-            self.employees_tree.append(employee)
+            self.current_tree.append(employee)
             self._add_subordinates_to_tree(employee)
 
     def clear_tree(self):
-        self.employees_tree.clear()
+        self.current_tree.clear()
--- employees/views.py
+++ employees/views.py
@@ -21,7 +21,11 @@ tr_tag_colors = [
 
 def page(request):
     employee_director = Employee.objects.get(hierarchy_level=1)
-    employee_tree = EmployeeTree(Employee.objects.all()).employees_tree
+    if not EmployeeTree.full_tree:
+        EmployeeTree.full_tree = EmployeeTree(Employee.objects.all()).current_tree
+    employee_tree = EmployeeTree.full_tree.copy()
+    if employee_director in employee_tree:
+        employee_tree.remove(employee_director)
     template = loader.get_template('employees/mainpage.html')
     context = {
         'employee_director': employee_director,
@@ -34,11 +38,10 @@ def page(request):
 
 def searchpage(request):
     employee_director = Employee.objects.get(hierarchy_level=1)
+    if not EmployeeTree.full_tree:
+        EmployeeTree.full_tree = EmployeeTree(Employee.objects.all()).current_tree
+    employee_tree = EmployeeTree.full_tree.copy()
     searchform = SearchForm
-    if not Employee.employees_tree:
-        employee_tree = Employee.get_tree()
-    else:
-        employee_tree = Employee.employees_tree
     template = loader.get_template('employees/searchpage.html')
     context = {
         'employee_director': employee_director,
@@ -52,24 +55,32 @@ def searchpage(request):
 def post_searchform(request):
     if request.method == "POST":
         filters = {
-            'first_name' : request.POST["first_name"],
-            'last_name' : request.POST["last_name"],
-            'middle_name' : request.POST["middle_name"],
-            'position' : request.POST["position"],
-            'employment_date' : request.POST["employment_date"],
-            'salary' : request.POST["salary"],
+            'first_name': request.POST["first_name"],
+            'last_name': request.POST["last_name"],
+            'middle_name': request.POST["middle_name"],
+            'position': request.POST["position"],
+            'employment_date': request.POST["employment_date"],
+            'salary': request.POST["salary"],
         }
-        if not Employee.employees_tree:
-            employee_tree = Employee.get_tree()
-        else:
-            employee_tree = Employee.employees_tree
+        filtered_queryset = get_filtered_queryset(filters)
+        filtered_tree = EmployeeTree(filtered_queryset).current_tree
+        template = loader.get_template('employees/searchresults.html')
+        context = {
+            'employee_tree': filtered_tree,
+        }
+        return HttpResponse(template.render(context, request))
 
-       # filtered_tree = filter(def ,employee_tree)
-               # (first_name=first_name,
-                #                         last_name=last_name,
-                 #                        middle_name=middle_name,
-                  #                       position=position,
-                   #                      employment_date=employment_date,
-                    #                     salary=salary)
 
-       # return HttpResponse(filtered_tree)
+def get_filtered_queryset(filters):
+    first_name = filters['first_name']
+    last_name = filters['last_name']
+    middle_name = filters['middle_name']
+    position = filters['position']
+    employment_date = filters['employment_date']
+    salary = filters['salary']
+    return Employee.objects.filter(first_name__icontains=first_name,
+                                   last_name__icontains=last_name,
+                                   middle_name__icontains=middle_name,
+                                   position__icontains=position,
+                                   employment_date__icontains=employment_date,
+                                   salary__icontains=salary)
