c2538c6a694cea33610d0eb5953d2a76214c6f6a
Severian <gordoncloud@inbox.ru>
2022-12-11 13:04:25 +0300

    Corrected description of the method get_tree

--- employees/models.py
+++ employees/models.py
@@ -24,7 +24,8 @@ class Employee(models.Model):
         '''
         Рекурсивно проходит по объектам employee,
         возвращает список объектов следующего вида:
-        [1, 1/1, 1/1/1, 1/1/2, 1/1/3, 1/2, 1/2/1, 1/2/1/1 ...]
+        [2, 2/1, 2/1/1, 2/1/2, 2/1/3, 2/2, 2/2/1, 2/2/1/1 ...]
+        (иерархия сотрудников фирмы, начиная с руководителей 2-го уровня)
         '''
         second_level_queryset = cls.objects.filter(hierarchy_level=2)
         for employee in second_level_queryset:
